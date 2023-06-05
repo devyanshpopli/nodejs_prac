ce21af92b631b6baaeffe363d7ac2d451329e83d
devyanshpopli <devyanshpopli40@gmail.com>
2023-06-05 23:31:24 +0530

    changes made in app.js and also moved the index.hbs and login.hbs from layout folder to views folder

--- app.js
+++ app.js
@@ -1,107 +1,136 @@
-const express = require('express');
-const session = require('express-session')
-const hbs = require('express-handlebars')
-const mongoose = require('mongoose')
-const passport = require('passport')
-const localStrategy = require('passport-local').Strategy
-const bcrypt = require('bcrypt')
-const app = express();
+const express			= require('express');
+const session			= require('express-session');
+const hbs				= require('express-handlebars');
+const mongoose			= require('mongoose');
+const passport			= require('passport');
+const localStrategy		= require('passport-local').Strategy;
+const bcrypt			= require('bcrypt');
+const app				= express();
 
 mongoose.connect("mongodb://localhost:27017/node-auth-yt", {
-useNewUrlParser: true,
-useUnifiedTopology: true
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
 
 const UserSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    }
+	username: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
 });
-const User = mongoose.model('User',UserSchema);
+
+const User = mongoose.model('User', UserSchema);
 
 
-//middleware
-app.engine('hbs', hbs({extname:'.hbs'}));
-app.set('view engine','hbs');
+// Middleware
+app.engine('hbs', hbs.engine({
+     extname: '.hbs' }));
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 app.use(session({
-    secret:"verygoodsecret",
-    resave:"false",
-    saveUninitialized:true
-}))
-app.use(express.urlencoded({extended:false}));
+	secret: "verygoodsecret",
+	resave: false,
+	saveUninitialized: true
+}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-//passport.js
 
+// Passport.js
 app.use(passport.initialize());
-app.use(passport.session())
-passport.serializeUser(function (user,done){
-    done(null,user.id);
-})
+app.use(passport.session());
 
-passport.deserializeUser(function (id,done){
-    User.findById(id,function(err,user){
-        done(err,user);
-    });
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
 });
 
-passport.use(new localStrategy(function(username,password,done){
-    username.findOne({username:username},function(err,user){
-        if(err){return done(err);}
-        if(!user){
-            return done(null,false,{message:'incorrect username.'})
-        }
-        bcrypt.compare(password,user.password,function(err,res){
-            if(err) return done(err);
+passport.deserializeUser(function (id, done) {
+	User.findById(id, function (err, user) {
+		done(err, user);
+	});
+});
 
-            if(res==false){
-                return done(null,false,{message:'Incorrect password.'});
-            }
-            return done(null,user);
-        });
-    });
+passport.use(new localStrategy(function (username, password, done) {
+	User.findOne({ username: username }, function (err, user) {
+		if (err) return done(err);
+		if (!user) return done(null, false, { message: 'Incorrect username.' });
+
+		bcrypt.compare(password, user.password, function (err, res) {
+			if (err) return done(err);
+			if (res === false) return done(null, false, { message: 'Incorrect password.' });
+			
+			return done(null, user);
+		});
+	});
 }));
 
-function isLoggedIn(req,res,next){
-    if(req.isAuthenticated()) return next();
-    res.redirect('/login');
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated()) return next();
+	res.redirect('/login');
 }
 
-//routes
-app.get('/',isLoggedIn,(req,res)=>{
-    res.render("index",{title:"Home"});
+function isLoggedOut(req, res, next) {
+	if (!req.isAuthenticated()) return next();
+	res.redirect('/');
+}
+
+// ROUTES
+app.get('/', isLoggedIn, (req, res) => {
+	res.render("index", { title: "Home" });
 });
 
-app.get('/login', (req,res)=>{
-    res,render('login',{title:"Login"})
-})
+app.get('/about', (req, res) => {
+	res.render("index", { title: "About" });
+});
 
-//setup our admin user
+app.get('/login', isLoggedOut, (req, res) => {
+	const response = {
+		title: "Login",
+		error: req.query.error
+	}
+
+	res.render('login', response);
+});
+
+app.post('/login', passport.authenticate('local', {
+	successRedirect: '/',
+	failureRedirect: '/login?error=true'
+}));
+
+app.get('/logout', function (req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
+// Setup our admin user
 app.get('/setup', async (req, res) => {
-const exists = await User.exists({ username: "admin" });
-if (exists) {
-res.redirect('/login');
-return;
-};
-bcrypt.genSalt (10, function (err, salt) {
-if (err) return next(err);
-bcrypt.hash ("mypassword", salt, function (err, hash) {
-if (err) return next(err);
-const newAdmin = new User({
-username: "admin",
-password: hash
-});
-newAdmin.save();
-res.redirect('/login');
-});
-});
+	const exists = await User.exists({ username: "admin" });
+
+	if (exists) {
+		res.redirect('/login');
+		return;
+	};
+
+	bcrypt.genSalt(10, function (err, salt) {
+		if (err) return next(err);
+		bcrypt.hash("pass", salt, function (err, hash) {
+			if (err) return next(err);
+			
+			const newAdmin = new User({
+				username: "admin",
+				password: hash
+			});
+
+			newAdmin.save();
+
+			res.redirect('/login');
+		});
+	});
 });
 
-app.listen(3000,()=>{
-    console.log("Listening on port 3000");
-})
+app.listen(3000, () => {
+	console.log("Listening on port 3000");
+});
